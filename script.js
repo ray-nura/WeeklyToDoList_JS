c34c3659f997cf4b3f072687654c31a96bede2d5
Nur <raynura182@gmail.com>
2022-02-07 22:06:10 -0800

    change a theme function wokk now and + btn also

--- script.js
+++ script.js
@@ -11,23 +11,28 @@ const magicSkyData1 = magicSkyData();
 const mountainData1 = mountainData();
 const taskData = task();
 
-
 const main = document.querySelector('.main')
+const container = document.querySelector('.container')
+const bgImgTheme = document.querySelector('#bgImg');
+const taskInput = document.querySelector('.fa-plus-square');
+const cancelBtn = document.querySelector('#cancel');
+
 displayImg(moonData1, taskData)
 // displayImg(mountainData1, taskData)
 // displayImg(flowerData1, taskData)
 // displayImg(greenData1, taskData)
 // displayImg(magicSkyData1, taskData)
 
-
-
 function displayImg(data, task) {
-    const slideContainer = document.createElement('div')
-    slideContainer.className = 'container'
-    slideContainer.innerHTML = `
+
+    // first cleaning div class="container" 
+    container.innerHTML = ''
+    // then create a new 
+    // const slideContainer = document.createElement('div')
+    // slideContainer.className = 'container'
+    container.innerHTML = `
        ${data.map(el => `
        <div class="slide" id="${el.id}"
-
        style="background-image: url('${el.imgURL}')">
        <h3> ${el.name}</h3>
        <div class="slideTaskDay">
@@ -36,7 +41,6 @@ function displayImg(data, task) {
        ${task.map(t =>
         `
         <li ${t.id}><span>${t.time}</span><span class="taskValue">${t.task}</span><i class="fas fa-edit"></i><i class="fas fa-check"></i><i class="fas fa-trash"></i></li>
-        
         `
     ).join('')}
          <li><span>8.00</span><span class="taskValue">some-some-some-task</span><i class="fas fa-edit"></i><i class="fas fa-check"></i><i class="fas fa-trash"></i></li>
@@ -52,7 +56,7 @@ function displayImg(data, task) {
      </div>
     `
     ).join('')}`
-    main.appendChild(slideContainer)
+    // container.appendChild(slideContainer)
 
     const slides = document.querySelectorAll('.slide')
     for (const slide of slides) {
@@ -67,3 +71,31 @@ function displayImg(data, task) {
         })
     }
 }
+
+bgImgTheme.addEventListener('change', (e) => {
+    const imgData = e.target.value
+    console.log(imgData)
+    switch (imgData) {
+        case "moonData1": displayImg(moonData1, taskData);
+            break;
+        case "mountainData1": displayImg(mountainData1, taskData);
+            break;
+        case "flowerData1": displayImg(flowerData1, taskData);
+            break;
+        case "greenData1": displayImg(greenData1, taskData);
+            break;
+        case "magicSkyData1": displayImg(magicSkyData1, taskData);
+            break;
+        default: displayImg(moonData1, taskData);
+    }
+})
+
+taskInput.addEventListener('click', (e)=>{
+    e.preventDefault();
+    document.querySelector('section').style.display='block'
+})
+cancelBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    document.querySelector('section').style.display='none'
+
+})
